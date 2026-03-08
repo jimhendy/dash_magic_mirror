25f91c9436cd8678c2d4c1012927ec110ce741c6
jimhendy <jimhendy88@gmail.com>
2026-03-08 21:08:50 +0000

    Increase HTTP timeout from 10 to 30 seconds

--- src/components/weather/constants.py
+++ src/components/weather/constants.py
@@ -2,5 +2,5 @@
 
 BASE_URL: str = "http://api.weatherapi.com/v1"
 FORECAST_DAYS: int = 3
-HTTP_TIMEOUT: int = 10
+HTTP_TIMEOUT: int = 30
 HOURLY_WINDOW_DAYS: int = 2  # number of days to gather hourly forecast
